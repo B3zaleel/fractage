df6018576a8db0321ec46e575b1e49802f523b70
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-27 12:47:19 +0100

    Add newton-basin to application routes

--- src/config/router.go
+++ src/config/router.go
@@ -14,6 +14,7 @@ func AddRoutes(app *iris.Application) {
 	app.Get("/hopalong", controllers.GetHopalong)
 	app.Get("/julia-set", controllers.GetJuliaSet)
 	app.Get("/mandelbrot-set", controllers.GetMandelbrotSet)
+	app.Get("/newton-basin", controllers.GetNewtonBasin)
 	app.Get("/sierpinski-carpet", controllers.GetSierpinskiCarpet)
 	app.Get("/sierpinski-triangle", controllers.GetSierpinskiTriangle)
 }
