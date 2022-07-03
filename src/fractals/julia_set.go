f9f64b3884b02ed91f726c0928a8548134196965
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-07-03 05:08:47 +0100

    Add support for generating Julia set of different series

--- src/fractals/julia_set.go
+++ src/fractals/julia_set.go
@@ -7,21 +7,52 @@ import (
 	"io"
 	"math"
 	"math/cmplx"
+	"strings"
 
 	"github.com/B3zaleel/fractage/src/helpers"
 )
 
 // Properties of a Julia set image.
 type JuliaSet struct {
-	Width         int
-	Height        int
-	ColorPalette  helpers.ColorPalette
-	MaxIterations int
-	C             complex128
-	BailOut       float64
-	Region        helpers.Rect
-	Background    color.RGBA
+	Width              int
+	Height             int
+	ColorPalette       helpers.ColorPalette
+	MaxIterations      int
+	C                  complex128
+	BailOut            float64
+	Region             helpers.Rect
+	SeriesFunctionName string
+	Background         color.RGBA
 }
+
+// Creates a function that computes the sum of c and the absolute value of
+// the 4th power of a trigonometric function for a given z.
+func absTrig(props *JuliaSet, trigFxn func(complex128) complex128) func(complex128) complex128 {
+	abs := func(c complex128) complex128 {
+		return complex(math.Abs(real(c)), math.Abs(imag(c)))
+	}
+	return func(z complex128) complex128 {
+		return abs(cmplx.Pow(trigFxn(z), 4)) + props.C
+	}
+}
+
+var (
+	JULIA_SET_SERIES = map[string]func(*JuliaSet) func(complex128) complex128{
+		"classic": func(props *JuliaSet) func(complex128) complex128 {
+			return func(z complex128) complex128 { return z*z + props.C }
+		},
+		"abs_sin4":   func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Sin) },
+		"abs_cos4":   func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Cos) },
+		"abs_tan4":   func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Tan) },
+		"abs_cot4":   func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Cot) },
+		"abs_sinh4":  func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Sinh) },
+		"abs_cosh4":  func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Cosh) },
+		"abs_tanh4":  func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Tanh) },
+		"abs_asinh4": func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Asinh) },
+		"abs_acosh4": func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Acosh) },
+		"abs_atanh4": func(props *JuliaSet) func(complex128) complex128 { return absTrig(props, cmplx.Atanh) },
+	}
+)
 
 // Writes the Julia set image to the given output.
 func (props *JuliaSet) WriteImage(output io.Writer) error {
@@ -51,13 +82,14 @@ func (props *JuliaSet) render(img *image.RGBA) error {
 	}
 	var pixelColor color.RGBA
 	var n int
+	seriesFunction := JULIA_SET_SERIES[props.SeriesFunctionName](props)
 	for y := 0; y < int(height); y++ {
 		for x := 0; x < int(width); x++ {
 			n = 0
 			Z := complex(xOffset+float64(x)*step, yOffset+float64(y)*step)
 			seriesValue := math.Exp(-cmplx.Abs(Z))
 			for (n < props.MaxIterations) && (cmplx.Abs(Z) < props.BailOut) {
-				Z = Z*Z + props.C
+				Z = seriesFunction(Z)
 				seriesValue += math.Exp(-cmplx.Abs(Z))
 				n++
 			}
@@ -76,4 +108,15 @@ func (props *JuliaSet) render(img *image.RGBA) error {
 		}
 	}
 	return nil
+}
+
+// Checks if a function name exists in the set of JULIA_SET_PARTIAL_FUNCTIONS names.
+func IsValidJuliaSetPartialFunction(txt string) bool {
+	fxnName := strings.Trim(txt, helpers.WHITESPACE_CUTSET)
+	for name, _ := range JULIA_SET_PARTIAL_FUNCTIONS {
+		if name == fxnName {
+			return true
+		}
+	}
+	return false
 }
